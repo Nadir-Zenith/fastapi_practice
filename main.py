ccad963a632331151a4cbb236aa8faca755f3e2c
Bijaya Khadka <zen.kalon@gmail.com>
2025-03-24 11:45:48 +0545

    query parameters

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
-from enum import Enum
+from schemas import GenreURLChoices, Band
 
 
 app = FastAPI()
 
-class GenreURLChoices(Enum):
-    ROCK = 'rock'
-    ELECTRONIC = 'electronic'
-    METAL = 'metal'
-    HIP_HOP = 'hip-hop'
+
 
 
 BANDS = [
     {'id' : 1, 'name' : 'The Kinks', 'genre' : 'Rock'},
     {'id' : 2, 'name' : 'Aphex Twin', 'genre' : 'Electronic'},
-    {'id' : 3, 'name' : 'Black Sabbath', 'genre' : 'Metal'},
+    {'id' : 3, 'name' : 'Black Sabbath', 'genre' : 'Metal', 'albums':[{'title': 'Master of Reality', 'release_date': '1971-07-21'}]},
     {'id' : 4, 'name' : 'Wu-Tang Clan', 'genre' : 'Hip-Hop'},
 ]
 
 
 @app.get("/bands")
-async def bands() -> list[dict]:
-    return BANDS
+async def bands(genre: GenreURLChoices | None = None) -> list[Band]:
+    if genre:
+        return [
+            Band(**b) for b in BANDS if b['genre'].lower() == genre.value
+        ]
+    return [Band(**b) for b in BANDS]
 
 
 @app.get('/bands/{band_id}')
-async def band(band_id: int) -> dict:
-    band = next((b for b in BANDS if b['id'] == band_id), None)
+async def band(band_id: int) -> Band:
+    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
     if band is None:
         # status code 404
         raise HTTPException(status_code=404, detail='Band not found')
     return band
 
 
-@app.get('/bands/genre/{genre}')
-async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
-    return [
-        b for b in BANDS if b['genre'].lower() == genre.value
-    ]
+
 
 
 
