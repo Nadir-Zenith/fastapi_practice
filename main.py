a7c8e19d03f1e775d9ff45b2a0e7e77622510d5b
Bijaya Khadka <zen.kalon@gmail.com>
2025-03-24 22:09:01 +0545

    sql db in the app

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException, Path, Query
-from schemas import GenreURLChoices, BandWithID, BandCreate
+from fastapi import FastAPI, HTTPException, Path, Query, Depends
+from contextlib import asynccontextmanager
+from models import GenreURLChoices, Band, BandCreate, Album
 from typing import Annotated
+from db import init_db, get_session
+from sqlmodel import Session, select
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 
 
@@ -17,8 +25,13 @@ BANDS = [
 
 
 @app.get("/bands")
-async def bands(genre: GenreURLChoices | None = None, q: Annotated[str | None, Query(max_length=10)] = None ) -> list[BandWithID]:
-    band_list = [BandWithID(**b) for b in BANDS]
+async def bands(
+    genre: GenreURLChoices | None = None,
+    q: Annotated[str | None, Query(max_length=10)] = None,
+    session: Session = Depends(get_session)
+) -> list[Band]:
+    
+    band_list = session.exec(select(Band)).all()
     if genre:
         band_list = [
             b for b in band_list if b.genre.value.lower() == genre.value
@@ -29,8 +42,11 @@ async def bands(genre: GenreURLChoices | None = None, q: Annotated[str | None, Q
 
 
 @app.get('/bands/{band_id}')
-async def band(band_id: Annotated[int, Path(title="The band ID")]) -> BandWithID:
-    band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
+async def band(
+    band_id: Annotated[int, Path(title="The band ID")], 
+    session: Session = Depends(get_session)
+) -> Band:
+    band = session.get(Band, band_id)
     if band is None:
         # status code 404
         raise HTTPException(status_code=404, detail='Band not found')
@@ -38,10 +54,22 @@ async def band(band_id: Annotated[int, Path(title="The band ID")]) -> BandWithID
 
 
 @app.post("/bands/")
-async def create_band(band_data: BandCreate) -> BandWithID:
-    id = BANDS[-1]['id'] + 1
-    band = BandWithID(id=id, **band_data.model_dump()).model_dump()
-    BANDS.append(band)
+async def create_band(
+    band_data: BandCreate,
+    session: Session = Depends(get_session)
+    )-> Band:
+    band = Band(name=band_data.name, genre=band_data.genre)
+    session.add(band)
+
+    if band_data.albums:
+        for album in band_data.albums:
+            album_obj = Album(
+                title=album.title, release_date=album.release_date, band=band
+                )
+            session.add(album_obj)
+
+    session.commit()
+    session.refresh(band)
     return band
 
 
